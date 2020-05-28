79429d8d447c8d0dce98d6aac123cbb053f07d3a
Eliseo <eliseonunezvega@gmail.com>
2020-05-27 22:21:46 -0700

    almost done with RRT needs testing

--- src/autonomous_turtlebot/scripts/RRT_node.py
+++ src/autonomous_turtlebot/scripts/RRT_node.py
@@ -16,11 +16,14 @@ class RRT():
 		rospy.Subscriber('target_pose', Float32MultiArray, self.handleTargetPose)
 		self.map_width = 0
 		self.map_height = 0
-		self.random_range = 100
-		self.dist_threshold = 7
+		self.random_range = 15
+		self.dist_threshold = 0.25
+		self.destination_point = None
 		self.isolated_map = None
+		self.occupancy_grid = None
 		self.found_path = False
 		self.path_tree = []
+		self.final_path = []
 		
 
 
@@ -29,7 +32,11 @@ class RRT():
 		return indx
 
 	def handleMap(self, map_data):
-
+		
+		# getting occupancy data
+		if self.occupancy_grid == None:
+			self.occupancy_grid = map_data.data
+		
 		# getting map dimensions and resolution 
 		if self.map_width == None and self.map_height == None:
 			self.map_width = map_data.info.width
@@ -46,11 +53,21 @@ class RRT():
 		pass
 	
 	def handleTargetPose(self, target_pose):
-		pass
+		if self.destination_pose == None:
+			self.destination_pose = (target_pose[0], target_pose[1])
+	
 
 	def random_configuration(self):
-		random_x = random.random()*self.random_range
-		random_y = random.random()*self.random_range
+		x_sign = random.random() 
+		y_sign = random.random()
+		if x_sign > .5:
+			x_sign = -1
+		if y_sign > .5:
+			y_sign = -1
+
+		random_x = x_sign*random.random()*self.random_range
+		random_y = y_sign*random.random()*self.random_range
+
 		return (x,y)
 
 	def get_euclidian_distance(point1, point2):
@@ -59,14 +76,25 @@ class RRT():
 
 	def nearest_vertex(self, random_point):
 		distances = []
+		nearest_distance = None
+
+		if len(self.path_tree) == 0:
+			nearest_node = None
+			nearest_distance = 0
+			return nearest_node, distance
+		
+
 		for i in self.path_tree:
 			x_i = i[1][0]
 			y_i = i[1][1]
 			new_distance = self.get_euclidian_distance((x_i,y_i), random_point)
 			distances.append(new_distance)
+
 		nearest_node = distances.index(min(distances))
+		nearest_distance = min(distance)
 		distances.clear()
-		return nearest_node, distance
+
+		return nearest_node, nearest_distance
 	
 	def new_configuration(self, random_point, nearest_neighbor, distance):
 		if distance > self.dist_threshold:
@@ -108,18 +136,35 @@ class RRT():
 			self.path_tree.append(final_node)
 
 
+	def grid_value(self,point):
+		half_width = (self.map_width/2)
+		scaled_y = (point[1]/sel.map_resolution)
+		scaled_x = (point[0]/sel.map_resolution)
+		index = half_width*(scaled_y + half_width) + (-scaled_x + 1024)
+		return self.occupancy_grid[index]
 
+	def retrace_path(self):
+		final_path = []
+		parent = self.path_tree[-1]
+
+		while parent != None:
+			final_path.append(parent[1])
+			parent = self.path_tree[parent[0]]
+		
+		return final_path
 
 	def doRRT(self):
 
 		while not self.found_path:
+
+			# getting random point configuration
 			random_point = self.random_configuration()
 
-			# making sure we only choose free points
+			# making sure we only choose unobstructed points
 			while self.grid_value(random_point) != 0:
 				random_point = self.random_configuration()
 			
-			# get nearest node and ditance to it
+			# get nearest node and distance to it
 			nearest_node, distance = self.nearest_vertex(random_point)
 			
 			# get new configuration point 
@@ -130,6 +175,10 @@ class RRT():
 			# adding the new point to path tree
 			self.add_vertex(new_node)
 			
+		
+		self.final_path = self.retrace_path()
+
+		rospy.loginfo(self.final_path)
 			
 			
 			
